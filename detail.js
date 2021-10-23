3d495ef2778dec065dc5437bfd6d1f5b62ac05d6
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2021-10-23 19:55:06 +0530

    added all data by js and some of fix

---
+++ detail.js
@@ -0,0 +1,27 @@
+let detail = {
+
+    name: "",
+    hamePageIntro: "",
+
+    aboutIntro: "",
+    aboutDetailPara1: "",
+    aboutDetailPara2: "",
+
+    prjDetails1: "",
+    prjDetails2: "",
+    prjDetails3: "",
+
+    prjTitle1: "",
+    prjTitle2: "",
+    prjTitle3: "",
+
+    prjLink1: "",
+    prjLink2: "",
+    prjLink3: "",
+
+    socialLink1: "",
+    socialLink2: "",
+    socialLink3: "",
+    socialLink4: "",
+
+};
--- main.js
+++ main.js
@@ -1,3 +1,57 @@
+const homeName = document.querySelector('.homeName');
+const homeIntro = document.querySelector('.homeIntro');
+
+const aboutIntro = document.querySelector('.aboutIntro');
+const aboutDetail1 = document.querySelector('.aboutDetail1');
+const aboutDetail2 = document.querySelector('.aboutDetail2');
+
+const prjDetails1 = document.querySelector('.prjDetails1');
+const prjDetails2 = document.querySelector('.prjDetails2');
+const prjDetails3 = document.querySelector('.prjDetails3');
+
+const prjTitle1 = document.querySelector('.prjTitle1');
+const prjTitle2 = document.querySelector('.prjTitle2');
+const prjTitle3 = document.querySelector('.prjTitle3');
+
+const prjLink1 = document.querySelector('.prjLink1');
+const prjLink2 = document.querySelector('.prjLink2');
+const prjLink3 = document.querySelector('.prjLink3');
+
+const socialLink1 = document.querySelector('.socialLink1');
+const socialLink2 = document.querySelector('.socialLink2');
+const socialLink3 = document.querySelector('.socialLink3');
+const socialLink4 = document.querySelector('.socialLink4');
+
+
+
+
+homeName.innerHTML = `<span>I'M</span> Jon Doe`;
+homeIntro.innerHTML = ``;
+
+aboutIntro.innerHTML = ``;
+
+
+aboutDetail1.innerHTML = ``;
+aboutDetail2.innerHTML = ``;
+
+
+prjDetails1.innerHTML = ``;
+prjDetails2.innerHTML = ``;
+prjDetails3.innerHTML = ``;
+
+prjTitle1.innerHTML = ``;
+prjTitle2.innerHTML = ``;
+prjTitle3.innerHTML = ``;
+
+prjLink1.href = ``;
+prjLink2.href = ``;
+prjLink3.href = ``;
+
+socialLink1.href = ``;
+socialLink2.href = ``;
+socialLink3.href = ``;
+socialLink4.href = ``;
+
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
 
